0fbaa81f8f1b29d51b4d6011f2af0d7221ab7d1b
eduwr <eduwric@gmail.com>
2022-06-12 23:45:03 -0300

    feat: :sparkles: presentation section responsivity

--- src/components/Presentation/index.tsx
+++ src/components/Presentation/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export const Presentation = () => (
-  <section className="flex justify-center gap-8 items-center py-16 px-8 w-full h-1/2-screen relative bg-light">
+  <section className="flex flex-col md:flex-row justify-center gap-8 items-center py-16 md:px-8 w-full md:h-1/2-screen relative bg-light">
     <div className="h-96 w-96 rounded-full relative overflow-hidden">
       <Image
         src="/assets/images/profile.png"
@@ -10,15 +10,15 @@ export const Presentation = () => (
         objectFit="cover"
       />
     </div>
-    <div className="w-1/2 h-full flex flex-col relative">
-      <h3 className="text-5xl text-yellow font-bold mb-5 uppercase">
+    <div className="md:w-1/2 px-5 h-full flex flex-col relative">
+      <h3 className="text-5xl text-center md:text-left text-yellow font-bold mb-5 uppercase">
         Quem é Vitória Cavassin?
       </h3>
       <p className="text-2xl text-grey font-thin text-justify">
         Mamãe do Tigue, diva, iluminada, esotérica, aquariana nata e arquiteta
         renomada.
       </p>
-      <button className="self-end w-[75%] py-4 px-11 mt-4 text-2xl text-yellow uppercase border-2 border-yellow rounded-lg hover:text-white hover:bg-yellow transition-all duration-300">
+      <button className="md:self-end md:w-[75%] py-4 px-11 mt-4 text-2xl text-yellow uppercase border-2 border-yellow rounded-lg hover:text-white hover:bg-yellow transition-all duration-300">
         Entre em contato
       </button>
     </div>
