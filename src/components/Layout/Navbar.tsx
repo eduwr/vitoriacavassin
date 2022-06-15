1fa4af6900a0539f72e759a5f5e89a0e82ecd9e4
eduwr <eduwric@gmail.com>
2022-06-15 00:13:05 -0300

    style: :lipstick: remove unecessary mt

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export const Navbar = ({ pages, showNavModal }: Props) => {
   const { asPath } = useRouter();
-  const navClass = showNavModal ? "mt-8 flex flex-col" : "mt-8 sm:flex hidden";
+  const navClass = showNavModal ? "flex flex-col" : "mt-8 sm:flex hidden";
   const listClass = showNavModal
     ? "absolute opacity-95 flex flex-col divide-y-2 pt-20 divide-black top-0 left-0 right-0 bottom-0 bg-light"
     : "flex divide-x-2 divide-black";
