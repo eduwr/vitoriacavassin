7a3c6ca5a4396a871c835c521e8e450538251ed1
eduwr <eduwric@gmail.com>
2022-06-14 22:12:13 -0300

    style: :lipstick: fixes project card size

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -13,15 +13,22 @@ interface Props {
 
 export const ProjectCard = ({ project }: Props) => {
   return (
-    <div className="flex flex-col items-center justify-between w-1/3 px-8">
-      <Image
-        src={project.photoUrl}
-        alt={project.title}
-        width={400}
-        height={200}
-      />
-      <h6 className="text-2xl font-bold my-2">{project.title}</h6>
-      <p className="text-base font-thin text-center">{project.description}</p>
+    <div
+      className="flex flex-col items-center justify-between w-96 min-w-fit px-8 pb-5"
+      style={{ minWidth: "24rem" }}
+    >
+      <div className="relative w-96 h-48">
+        <Image
+          src={project.photoUrl}
+          alt={project.title}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <h6 className="text-2xl my-2">{project.title}</h6>
+      <p className="line-clamp-3 text-base font-thin text-center">
+        {project.description}
+      </p>
     </div>
   );
 };
