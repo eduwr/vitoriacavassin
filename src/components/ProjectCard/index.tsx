59a3b9216fda9bc24377a1e1a0542d0cfcb9bc24
eduwr <eduwric@gmail.com>
2022-06-16 17:30:29 -0300

    feat: :sparkles: load project details data

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -8,24 +8,20 @@ interface Props {
   project: Project;
 }
 
-export const ProjectCard = ({ project }: Props) => {
-  const handleNavigation = () => {};
-
-  return (
-    <Link href={`/projects/${project.id}`} passHref>
-      <a className="shadow-xl flex flex-col items-center justify-between w-96 m-2 md:m-0 md:mx-2 pb-5">
-        <Image
-          src={project.pictures[0]}
-          alt={project.name}
-          width={384}
-          height={192}
-          objectFit="cover"
-        />
-        <h6 className="text-2xl my-2">{project.name}</h6>
-        <p className="line-clamp-3 text-base font-thin text-center">
-          {project.description}
-        </p>
-      </a>
-    </Link>
-  );
-};
+export const ProjectCard = ({ project }: Props) => (
+  <Link href={`/projects/${project.id}`} passHref>
+    <a className="shadow-xl flex flex-col items-center justify-between w-96 m-2 md:m-0 md:mx-2 pb-5">
+      <Image
+        src={project.pictures[0]}
+        alt={project.name}
+        width={384}
+        height={192}
+        objectFit="cover"
+      />
+      <h6 className="text-2xl my-2">{project.name}</h6>
+      <p className="line-clamp-3 text-base font-thin text-center">
+        {project.description}
+      </p>
+    </a>
+  </Link>
+);
--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -1,7 +1,25 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import db from "../../data/resources.json";
 
-const ProjectDetails: NextPage = (props) => {
-  return <div className="detail">Detail</div>;
+const projects = db.projects;
+
+const ProjectDetails: NextPage = () => {
+  const { query } = useRouter();
+
+  const project = projects.find((project) => project.id === Number(query.id));
+
+  if (!project) {
+    return <div>Project Not Found</div>;
+  }
+
+  return (
+    <div className="detail">
+      <h1>{project.name}</h1>
+      <span>{project.type}</span>
+      <p>{project.description}</p>
+    </div>
+  );
 };
 
 export default ProjectDetails;
