5856e13d43128d43801b6962ad6eb0d2e86d5530
eduwr <eduwric@gmail.com>
2022-06-24 22:10:51 -0300

    feat: centralize language button

--- src/components/LanguageButton/index.tsx
+++ src/components/LanguageButton/index.tsx
@@ -7,15 +7,16 @@ export const LanguageButton = () => {
 
   const localeTo = locale === "en-US" ? "pt-BR" : "en-US"
   const iconPath = locale === "en-US" ? "/icons/usa-flag-80.png" : "/icons/brazil-flag-80.png"
+  const altText = locale === "en-US" ? "English" : "Português"
+
   return (
     <div className="flex items-center">
       <p className="mr-3">Language:</p>
       <Link href={asPath} locale={localeTo}>
-        <a className="bg-yellow">
-          <Image width={32} height={32} src={iconPath}/>
+        <a className="flex items-center">
+          <Image alt={altText} width={32} height={32} src={iconPath}/>
         </a>
       </Link>
     </div>
-
   )
 }
