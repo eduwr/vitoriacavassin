3c503ef8c776db2216120efe59bad9f97629a183
eduwr <eduwric@gmail.com>
2022-06-26 15:31:35 -0300

    feat: create projects api endpoint

---
+++ src/pages/api/projects.ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import db from '../../data/resources.json';
+
+type ErrorResponse = { message: string }
+type Data = ReturnType<() => typeof db.projects>
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | ErrorResponse>
+) {
+
+  switch (req.method) {
+    case "GET":
+      return res.status(200).json(db.projects);
+    default:
+      return res.status(404).json({ message: "Bad Request" });
+  }
+}
