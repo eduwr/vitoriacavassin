0e3a97944caeebea3f948689f4bacd9fb7d44037
eduwr <eduwric@gmail.com>
2022-07-01 16:31:05 -0300

    feat: create base button

---
+++ src/components/BaseButton/index.tsx
@@ -0,0 +1,29 @@
+import { CSSProperties, ReactNode } from "react";
+import { Property } from "csstype";
+
+interface ButtonProps {
+  children: ReactNode;
+  outline?: boolean;
+  align?: Property.AlignSelf;
+  justify?: Property.JustifySelf;
+}
+
+
+const outlineStyle = "py-4 px-8 text-xl text-yellow uppercase border-2 border-yellow rounded-lg hover:text-white hover:bg-yellow transition-all duration-300"
+const buttonStyle = "py-4 px-8 text-xl text-white uppercase rounded-lg bg-yellow transition-all duration-300 opacity-80 hover:opacity-100"
+
+export const BaseButton = ({ children, outline, align, justify }: ButtonProps) => {
+  const alignment: CSSProperties = {
+    alignSelf: align,
+    justifySelf: justify
+  }
+
+  return (
+    <div style={alignment}>
+      <button
+        className={outline ? outlineStyle : buttonStyle}>
+        {children}
+      </button>
+    </div>
+  )
+}
